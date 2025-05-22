39f9524afcebc232c5617e29538e954f1e35cf4a
Deva1502 <devanshm929@gmail.com>
2025-05-22 14:25:09 +0530

    modify page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button variant={"destructive"}>Hii</Button>
+      <Button variant={"destructive"} className="flex items-center justify-center">Hii</Button>
     </div>
   );
 }
