637e5f5846c715f7533ed28267fd65f79139c27a
Deva1502 <devanshm929@gmail.com>
2025-05-26 18:50:43 +0530

    create product detail

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ecom";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "E-commerce application";
-export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more";           
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more";        
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)   
